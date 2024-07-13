9bfda3f3b648ba3a58452ca6132b16e060aa14a5
ak47607 <153388712+ak47607@users.noreply.github.com>
2024-07-14 03:44:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -69,13 +69,13 @@ module.exports = {
 
   menu: process.env.MENU || "", /**  Available @MENU @Schemes 1: Aztec_Md, 2: A17_Md, 3: Suhail-Md Default ---------- If Not Choose then it Randomely Pic One Of Them Each time **/
 
-  HANDLERS: process.env.PREFIX  || ".",
+  HANDLERS: process.env.PREFIX  || ",",
   BRANCH  : process.env.BRANCH  || "main",
   VERSION : process.env.VERSION || "1.3.8",
   caption : global.caption || "Hey I am here to help you." , // ```『 ᴘᴏᴡᴇʀᴇᴅ ʙʏ sᴜʜᴀɪʟ²²¹-ᴍᴅ 』```", //*『sᴜʙsᴄʀɪʙᴇ • sᴜʜᴀɪʟ ᴛᴇᴄʜ』*\n youtube.com/@suhailtechinfo0"),
  
-  author : process.env.PACK_AUTHER|| "",
-  packname: process.env.PACK_NAME || "",
+  author : process.env.PACK_AUTHER|| "AKASH",
+  packname: process.env.PACK_NAME || "AKASH-XD",
   botname : process.env.BOT_NAME  || "AKASH",
   ownername:process.env.OWNER_NAME|| "AKASH",
 
